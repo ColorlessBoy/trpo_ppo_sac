c68d688d8b83211412fa2b55c8e10ad6aac50457
colorlessboy <501868024@qq.com>
2020-01-20 21:53:03 +0800

    trpo_ppo result: HalfCheetah-v2 64workers.

--- exp.sh
+++ exp.sh
@@ -4,18 +4,6 @@ for seed in 0 ; # 100 200 ;
 do
     for env_name in HalfCheetah-v2 # Walker2d-v2 Ant-v2 Humanoid-v2 Swimmer-v2 HumanoidStandup-v2 Hopper-v2 ;
     do
-        for alg in local_ppo global_ppo ;
-        do
-            rm /home/peng/anaconda3/envs/pytorch/lib/python3.6/site-packages/mujoco_py/generated/*.lock
-            cd /home/peng/Documents/trpo_ppo_sac/ppo
-            echo "Experiment:" $alg "_" $env_name "_" $seed
-            python parallel_main.py --env_name $env_name \
-                                    --alg $alg \
-                                    --workers 64 \
-                                    --device cuda \
-                                    --seed $seed \
-                                    --reward_step $reward_step
-        done
         for alg in local_trpo local_trpo2 local_trpo3 hmtrpo global_trpo ;
         do
             rm /home/peng/anaconda3/envs/pytorch/lib/python3.6/site-packages/mujoco_py/generated/*.lock
@@ -26,7 +14,25 @@ do
                                     --workers 64 \
                                     --device cuda \
                                     --seed $seed \
-                                    --reward_step $reward_step
+                                    --reward_step 0 1 2 3 \
+                                    --master_addr 10.214.192.126 \
+                                    --node_size 2 \
+                                    --node_rank 1
+        done
+        for alg in local_ppo global_ppo ;
+        do
+            rm /home/peng/anaconda3/envs/pytorch/lib/python3.6/site-packages/mujoco_py/generated/*.lock
+            cd /home/peng/Documents/trpo_ppo_sac/ppo
+            echo "Experiment:" $alg "_" $env_name "_" $seed
+            python parallel_main.py --env_name $env_name \
+                                    --alg $alg \
+                                    --workers 64 \
+                                    --device cuda \
+                                    --seed $seed \
+                                    --reward_step 0 1 2 3 \
+                                    --master_addr 10.214.192.126 \
+                                    --node_size 2 \
+                                    --node_rank 1
         done
     done
 done
